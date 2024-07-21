87280e4647bace43f1ebb5aad7e46498c5059574
lorionMoreira <ldjmoreira@gmail.com>
2024-07-21 18:44:05 -0300

    testing 6

--- backend/src/main/java/com/nelioalves/cursomc/resources/WebhookController.java
+++ backend/src/main/java/com/nelioalves/cursomc/resources/WebhookController.java
@@ -20,7 +20,7 @@ public class WebhookController {
     @ResponseStatus(HttpStatus.OK)
     public void handleWebhook(@RequestHeader("X-Hub-Signature-256") String signature, @RequestBody String payload, HttpServletRequest request) {
         if (!validateSecret(signature, payload)) {
-            throw new SecurityException("Invalid secret5");
+            throw new SecurityException("Invalid secret6");
         }
 
         System.out.println("Received webhook payload2: " + payload);
