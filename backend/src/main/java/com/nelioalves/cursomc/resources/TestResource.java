647650ed6bac277e54f7122630949e6852918b17
lorionMoreira <ldjmoreira@gmail.com>
2024-07-28 15:36:51 -0300

    teste 1.3

--- backend/src/main/java/com/nelioalves/cursomc/resources/TestResource.java
+++ backend/src/main/java/com/nelioalves/cursomc/resources/TestResource.java
@@ -12,7 +12,7 @@ public class TestResource {
 
     @GetMapping("/version")
     public ResponseEntity<String> getAutomationSettings() {
-        String version = "version system test: 1.2";
+        String version = "version system test: 1.3";
 
         return ResponseEntity.ok(version); 
     }
