6847469549776e24739e32d58e6b790d403dc0a2
lorionMoreira <ldjmoreira@gmail.com>
2024-07-28 11:58:39 -0300

    verificando test

--- backend/src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ backend/src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/clientes/**",
 			"/auth/forgot/**",
+			"/api/test/**"
 
 	};
 
